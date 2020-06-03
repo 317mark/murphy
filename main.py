030fda23212cf5d2385e16863e94b610fb7b680e
Mark Williams <mark.williams027@gmail.com>
2020-06-02 22:55:56 -0400

    Created main.py

---
+++ main.py
@@ -0,0 +1,21 @@
+"""
+Script to be run at startup
+"""
+
+# Put rocket in Idle mode
+
+# Make connection with Arduino
+
+# Calibrate/Test sensors
+
+# Turn on the camera, begin recording, and
+
+# Indicate that sensors are reading out and properly calibrated (LED + Sound)
+
+# Make connection with Controller
+
+# Wait for signal
+
+# If controller dial is set to Arm, arm the rocket
+
+# If controller dial is set to Arm && the flip is switched, launch the rocket
